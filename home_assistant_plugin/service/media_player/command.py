abafc1f83714d398e72da8b1d69218ab009b04f4
Maja Massarini <maja.massarini@gmail.com>
2021-12-07 21:55:17 +0100

    Enhanced unit testing

--- home_assistant_plugin/service/media_player/command.py
+++ home_assistant_plugin/service/media_player/command.py
@@ -1,8 +1,20 @@
 import copy
+import home
 from home_assistant_plugin.message import Command as Parent
 
 
 class Play(Parent):
+    """
+    >>> import home
+    >>> import home_assistant_plugin
+
+    >>> cmd = home_assistant_plugin.service.media_player.command.Play.make(["bath_player"])
+    >>> old_state = home.appliance.sound.player.state.off.State()
+    >>> new_state = old_state.next(home.appliance.sound.player.event.forced.Event.On)
+    >>> msg = cmd.make_msgs_from(old_state, new_state)
+    >>> msg[0].service
+    'media_play'
+    """
 
     Message = {
         "type": "call_service",
@@ -13,7 +25,11 @@ class Play(Parent):
         }
     }
 
-    def make_msgs_from(self, old_state, new_state):
+    def make_msgs_from(self,
+                       old_state: [home.appliance.attribute.mixin.IsOn,
+                                   home.appliance.attribute.mixin.IsOff],
+                       new_state: [home.appliance.attribute.mixin.IsOn,
+                                   home.appliance.attribute.mixin.IsOff]):
         result = []
         if (old_state.is_on != new_state.is_on) and new_state.is_on:
             result = self.execute()
@@ -21,6 +37,17 @@ class Play(Parent):
 
 
 class Pause(Parent):
+    """
+    >>> import home
+    >>> import home_assistant_plugin
+
+    >>> cmd = home_assistant_plugin.service.media_player.command.Pause.make(["bath_player"])
+    >>> old_state = home.appliance.sound.player.state.forced.on.State()
+    >>> new_state = old_state.next(home.appliance.sound.player.event.forced.Event.Not)
+    >>> msg = cmd.make_msgs_from(old_state, new_state)
+    >>> msg[0].service
+    'media_pause'
+    """
 
     Message = {
         "type": "call_service",
@@ -31,7 +58,11 @@ class Pause(Parent):
         }
     }
 
-    def make_msgs_from(self, old_state, new_state):
+    def make_msgs_from(self,
+                       old_state: [home.appliance.attribute.mixin.IsOn,
+                                   home.appliance.attribute.mixin.IsOff],
+                       new_state: [home.appliance.attribute.mixin.IsOn,
+                                   home.appliance.attribute.mixin.IsOff]):
         result = []
         if (old_state.is_on != new_state.is_on) and not new_state.is_on:
             result = self.execute()
@@ -39,6 +70,19 @@ class Pause(Parent):
 
 
 class VolumeSet(Parent):
+    """
+    >>> import home
+    >>> import home_assistant_plugin
+
+    >>> cmd = home_assistant_plugin.service.media_player.command.VolumeSet.make(["bath_player"])
+    >>> old_state = home.appliance.sound.player.state.forced.on.State()
+    >>> new_state = old_state.next(home.appliance.sound.player.event.sleepy_volume.Event(15))
+    >>> msg = cmd.make_msgs_from(old_state, new_state)
+    >>> msg[0].service
+    'volume_set'
+    >>> msg[0].message["service_data"]["volume_level"]
+    15
+    """
 
     Message = {
         "type": "call_service",
@@ -50,12 +94,27 @@ class VolumeSet(Parent):
         }
     }
 
-    def make_msgs_from(self, old_state, new_state):
+    def make_msgs_from(self,
+                       old_state: home.appliance.attribute.mixin.Volume,
+                       new_state: home.appliance.attribute.mixin.Volume):
+        self.message["service_data"]["volume_level"] = new_state.volume
         result = self.execute()
         return result
 
 
 class ShuffleSet(Parent):
+    """
+    >>> import home
+    >>> import home_assistant_plugin
+
+    >>> cmd = home_assistant_plugin.service.media_player.command.ShuffleSet.make(["bath_player"])
+    >>> old_state = home.appliance.sound.player.state.forced.off.State()
+    >>> msg = cmd.make_msgs_from(old_state, old_state)
+    >>> msg[0].service
+    'shuffle_set'
+    >>> msg[0].message["service_data"]["shuffle"]
+    True
+    """
 
     Message = {
         "type": "call_service",
@@ -67,12 +126,27 @@ class ShuffleSet(Parent):
         }
     }
 
-    def make_msgs_from(self, old_state, new_state):
+    def make_msgs_from(self,
+                       old_state: home.appliance.State,
+                       new_state: home.appliance.State):
         result = self.execute()
         return result
 
 
 class SelectSource(Parent):
+    """
+    >>> import home
+    >>> import home_assistant_plugin
+
+    >>> cmd = home_assistant_plugin.service.media_player.command.SelectSource.make(["bath_player"])
+    >>> old_state = home.appliance.sound.player.state.forced.off.State()
+    >>> new_state = old_state.next(home.appliance.sound.player.event.playlist.Event("morning playlist"))
+    >>> msg = cmd.make_msgs_from(old_state, new_state)
+    >>> msg[0].service
+    'select_source'
+    >>> msg[0].message["service_data"]["source"]
+    'morning playlist'
+    """
 
     Message = {
         "type": "call_service",
@@ -84,13 +158,9 @@ class SelectSource(Parent):
         }
     }
 
-    def make_msgs_from(self, old_state, new_state):
+    def make_msgs_from(self,
+                       old_state: home.appliance.attribute.mixin.Playlist,
+                       new_state: home.appliance.attribute.mixin.Playlist):
+        self.message["service_data"]["source"] = new_state.playlist
         result = self.execute()
         return result
-
-    @classmethod
-    def make(cls, entity_id, source):
-        message = copy.deepcopy(cls.Message)
-        message["service_data"]["entity_id"] = entity_id
-        message["service_data"]["source"] = source
-        return cls(message)
